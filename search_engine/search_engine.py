e413409f9e5283a28299311e48221a4f29285be6
tzero <tzerorus@gmail.com>
2024-10-22 16:40:34 +0300

    feat: fuzzy search

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,12 +1,14 @@
 import re
 
-def search(lst, word):
+def search(lst, words):
     result = []
+    words_set = set(words.split())
     for d in lst:
         token = d['text'].lower()
         term = re.findall(r'\w+', token)
         term_str = ''.join(term)
-        if word in token.split() or word in term:
-            result.append({'id': d['id'], 'count': len(re.findall(word, token))})
-    result = [doc['id'] for doc in sorted(result, key=lambda d: d['count'], reverse=True)]
+        occurr_number = words_set.intersection(set(term) | set(token.split()))
+        if occurr_number:
+            result.append({'id': d['id'], 'occurr_number': len(occurr_number),'count': sum(i in words_set for i in term)})
+    result = [doc['id'] for doc in sorted(result, key=lambda d: (d['occurr_number'], d['count']), reverse=True)]
     return result
