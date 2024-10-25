7c582eb037b72f65d84a54fc4958f852960c7b2f
tzero <tzerorus@gmail.com>
2024-10-25 21:37:10 +0300

    feat: reverse search

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -3,10 +3,14 @@ import re
 def search(lst, words):
     result = []
     words_set = set(words.split())
+    index = {}
     for d in lst:
         token = d['text'].lower()
         term = re.findall(r'\w+', token)
-        term_str = ''.join(term)
+        #term_str = ''.join(term)
+        for word in term:
+            _index = index.setdefault(word, [])
+            _index.append(d['id'])
         occurr_number = words_set.intersection(set(term) | set(token.split()))
         if occurr_number:
             result.append({'id': d['id'], 'occurr_number': len(occurr_number),'count': sum(i in words_set for i in term)})
