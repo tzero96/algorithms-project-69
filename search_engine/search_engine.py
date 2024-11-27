080b7814af77c334edde23f5bcee4b8e5d48b335
tzero96 <161148628+tzero96@users.noreply.github.com>
2024-11-27 17:31:12 +0300

    feat tf-idf

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,18 +1,36 @@
 import re
 
-def search(lst, words):
-    result = []
-    words_set = set(words.split())
-    index = {}
-    for d in lst:
-        token = d['text'].lower()
-        term = re.findall(r'\w+', token)
-        #term_str = ''.join(term)
-        for word in term:
-            _index = index.setdefault(word, [])
-            _index.append(d['id'])
-        occurr_number = words_set.intersection(set(term) | set(token.split()))
-        if occurr_number:
-            result.append({'id': d['id'], 'occurr_number': len(occurr_number),'count': sum(i in words_set for i in term)})
-    result = [doc['id'] for doc in sorted(result, key=lambda d: (d['occurr_number'], d['count']), reverse=True)]
+def search(lst_docs, words):
+    term_words = re.findall(r"'?\b[0-9A-Za-z']+\b", words.lower())
+    def compute_inverted_index(docs):
+        inv_index = {}
+        for doc in docs:
+            token = doc['text'].lower()
+            term = re.findall(r"'?\b[0-9A-Za-z']+\b", token)
+            for word in term:
+                if word not in inv_index:
+                    inv_index[word] = set()
+                inv_index[word].add(doc['id'])
+        return inv_index
+
+    def compute_idf(inverted_index, num_docs, term_words_):
+        idf = {}
+        for word in term_words_:
+            idf[word] = log10(num_docs / len(inverted_index[word]))
+        return idf
+
+    def compute_tf_idf(docs, term_words_, idf):
+        tf_idf = []
+        for doc in docs:
+            token = doc['text'].lower()
+            term = re.findall(r"'?\b[0-9A-Za-z']+\b", token)
+            doc_tf_idf = {"doc_id": doc['id'],"tf_idf": sum([(term.count(word)/len(term))*idf[word] for word in term_words_])}
+            tf_idf.append(doc_tf_idf)
+        return tf_idf
+
+    inv_index = compute_inverted_index(lst_docs)
+    idf = compute_idf(inv_index, len(lst_docs), term_words)
+    tf_idf = compute_tf_idf(docs, term_words, idf)
+    result = [d['doc_id'] for d in sorted(tf_idf, key=lambda d: d['tf_idf'], reverse=True) if d['tf_idf'] != 0]
+    
     return result
